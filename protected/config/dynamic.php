84e60758538f9fba7f17aaaa911c6ef7cdb2d548
lavynia oliveira <94937715+lalaskka@users.noreply.github.com>
2021-11-30 16:03:34 -0300

    modules

--- protected/config/dynamic.php
+++ protected/config/dynamic.php
@@ -25,7 +25,7 @@
     ),
     'formatter' => 
     array (
-      'defaultTimeZone' => 'Europe/Berlin',
+      'defaultTimeZone' => 'America/Sao_Paulo',
     ),
   ),
   'params' => 
@@ -38,12 +38,12 @@
         'installer_database' => 'humhub',
       ),
     ),
-    'config_created_at' => 1638222878,
+    'config_created_at' => 1638227425,
     'horImageScrollOnMobile' => '1',
     'databaseInstalled' => true,
     'installed' => true,
   ),
   'name' => 'gamedot',
   'language' => 'pt',
-  'timeZone' => 'Europe/Berlin',
+  'timeZone' => 'America/Sao_Paulo',
 ); ?>
